cf21e6da0d359ae36fd641e33af05b8d66aae253
Luisxap98 <Luisxap98@users.noreply.github.com>
2024-05-08 02:17:17 -0500

    "Updated HTML and JS"

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 # Github: Luis-Perz
 # Credit: 
 GeekforGeeks , W3Schools, 
+#Icons from Flaticon.com
+html: <a href="https://www.flaticon.com/free-icons/html" title="html icons">Html icons created by Freepik - Flaticon</a>
+css: <a href="https://www.flaticon.com/free-icons/css" title="css icons">Css icons created by Pixel perfect - Flaticon</a>
+JavsScript: <a href="https://www.flaticon.com/free-icons/java" title="java icons">Java icons created by Pixel perfect - Flaticon</a>
+React: <a href="https://www.flaticon.com/free-icons/react" title="react icons">React icons created by Freepik - Flaticon</a>
+Php: <a href="https://www.flaticon.com/free-icons/php" title="php icons">Php icons created by Freepik - Flaticon</a>
+Python: <a href="https://www.flaticon.com/free-icons/python-file" title="python file icons">Python file icons created by Flat Icons - Flaticon</a>
+Java: <a href="https://www.flaticon.com/free-icons/java" title="java icons">Java icons created by Freepik - Flaticon</a>
+C++: <a href="https://www.flaticon.com/free-icons/coding" title="coding icons">Coding icons created by Freepik - Flaticon</a>
--- index.js
+++ index.js
@@ -1,1 +1,59 @@
+// loading icons from image folder
+var iconList = [
+    "html-5",
+    "css-3",
+    "java-script",
+    "java",
+    "python",
+    "php",
+    "c-",
+    "react"
+]
 
+var myIcons;
+var board = [];
+var rows = 4;
+var cols = 4;
+
+
+window.onload = function()  {
+    randIcons();
+    start();
+}
+// randomize icons
+function randIcons(){
+    // create icon pairs
+    myIcons = iconList.concat(iconList);
+    
+    // create random icon output
+    for (let i = 0; i < myIcons.length; i++){
+        // output a random number and multipy it by the length of 'myIcons' list
+        let j = Math.floor(Math.random() * myIcons.length);
+        // save initial icon i and replace it with random icon j
+        var loadedIcon = myIcons[i];
+        myIcons[i] = myIcons[j];
+        myIcons[j] = loadedIcon;
+    }
+    // verify that icons are outputting properly
+    console.log(myIcons);
+}
+
+function start() {
+    
+    for (let k  = 0; k < rows; k++) {
+        // creating rows for game board
+        let rows = [];
+        
+        for( let l = 0; l < cols; l++){
+            // remove the current icon from list
+            let iconLogos = myIcons.pop();
+            // insert next icon from list
+            rows.push(iconLogos);
+            // load icon images to game board
+            let icon = document.createElement("img");
+            icon.src = "images/" + iconLogos + ".png";
+            document.getElementById("board").append(icon);
+        }
+        board.push(rows);
+    }
+}
